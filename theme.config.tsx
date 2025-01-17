c7a9b9f9659343d25337ff834fbfbd6a4b69a5cc
highlander <highlander@keepkey.com>
2025-01-17 09:33:24 -0600

    doc work

--- theme.config.tsx
+++ theme.config.tsx
@@ -9,6 +9,9 @@ const config: DocsThemeConfig = {
   chat: {
     link: 'https://discord.com/invite/FDQEbB79N2',
   },
+  sidebar: {
+    defaultMenuCollapseLevel: 0, // Ensures all sections are collapsed by default
+  },
   docsRepositoryBase: 'https://github.com/keepkey/keepkey-docs-repo',
   footer: {
     text: 'KeepKey Hardware Wallet',
