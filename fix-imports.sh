06adc5a9d270c3126c00f1f684e633ca75789856
highlander <bithighlander@gmail.com>
2025-04-22 12:02:09 -0500

    docs work

--- fix-imports.sh
+++ fix-imports.sh
@@ -3,11 +3,8 @@
 # Find all MDX files that import Callout from nextra/components
 find ./pages -type f -name "*.mdx" -exec grep -l "import { Callout } from 'nextra/components'" {} \; | while read -r file; do
   echo "Fixing imports in $file"
-  # Replace the import statement
-  sed -i '' "s/import { Callout } from 'nextra\/components'/import { Callout } from '..\/..\/..\/components\/Callout'/g" "$file"
-  # Handle files at different directory depths
-  sed -i '' "s/import { Callout } from '..\/..\/..\/components\/Callout'/import { Callout } from '..\/..\/components\/Callout'/g" "$file"
-  sed -i '' "s/import { Callout } from '..\/..\/components\/Callout'/import { Callout } from '..\/components\/Callout'/g" "$file"
+  # Replace the import statement with the correct one for Nextra 2.2.14
+  sed -i '' "s/import { Callout } from 'nextra\/components'/import { Callout } from 'nextra-theme-docs'/g" "$file"
 done
 
 echo "Import fixes completed"
