e9368fb8c87a6bb4a196552aa292b0321d2d3803
highlander <bithighlander@gmail.com>
2025-04-27 19:36:39 -0500

    doc work

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,23 +1,29 @@
 import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
+import { KeepKeyUILogo } from './components/logo/keepkey-ui'
 
 // Define homepage URL from env
 const homepage = process.env.NEXT_PUBLIC_APP_HOME_URL || '/'
 
 const config: DocsThemeConfig = {
   logo: (
-    <img
-      src="/keepkey-logo.svg"
-      alt="KeepKey Logo"
-      style={{ height: '1.5em', marginRight: '0.5em' }}
-    />
+    <span style={{ display: 'flex', alignItems: 'center', gap: '0.5em' }}>
+      <KeepKeyUILogo style={{ height: '1.5em' }} />
+      <span style={{ fontWeight: 'bold' }}>KeepKey Documentation</span>
+    </span>
   ),
   logoLink: homepage,
   project: {
     link: 'https://github.com/keepkey',
   },
-  chat: {
-    link: 'https://discord.com/invite/FDQEbB79N2',
+  navbar: {
+    extraContent: (
+      <>
+        <a href="https://support.keepkey.info" target="_blank" rel="noopener noreferrer" style={{ marginLeft: '1em' /* Adjust spacing if needed */ }}>
+          Contact
+        </a>
+      </>
+    )
   },
   sidebar: {
     defaultMenuCollapseLevel: 0, // Ensures all sections are collapsed by default
